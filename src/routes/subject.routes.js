5713225a34708274d4dc47ebbf79959ded234eb8
JuanFe622 <juanf.herrerac@autonoma.edu.co>
2022-08-29 17:12:17 -0500

    Add new request methods in subject.routes file

--- src/routes/subject.routes.js
+++ src/routes/subject.routes.js
@@ -2,8 +2,27 @@ const express = require("express");
 const subject_model = require('../models/subject.model')
 const subject_routes = express.Router()
 
-subject_routes.post('/', (req, res)=>{})
-subject_routes.get('/', (req, res)=>{})
+subject_routes.post('/subject', (req, res)=>{
+    subject_model = subject_model(req.body);
+    new_subject
+    .save()
+    .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+});
+subject_routes.get('/', (req, res)=>{
+    subject_model
+    .find()
+    .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+});
 subject_routes.get('/:subjectId', (req, res)=>{})
 subject_routes.put('/:subjectId', (req, res)=>{})
 subject_routes.delete('/:subjectId', (req, res)=>{})
